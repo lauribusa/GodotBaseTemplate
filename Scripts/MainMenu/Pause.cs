a7e4bec1a38b613af7069f8147a40d80ba5ce12f
Lauri.busa <loki.lightbolt@gmail.com>
2023-01-03 20:34:42 +0100

    Added pause function, prevents game from resuming when hitting ESC while in a menu

--- Scripts/MainMenu/Pause.cs
+++ Scripts/MainMenu/Pause.cs
@@ -10,20 +10,50 @@ public partial class Pause : Control
 	public NodePath quitConfirmPath;
 	private ConfirmationDialog popupMenu;
 
+	private bool isInMenu = false;
+
 	public override void _Ready()
 	{
 		popupMenu = GetNode(quitConfirmPath) as ConfirmationDialog;
 		settingsPopup = GetNode(settingsMenuPath) as Window;
 	}
 
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustReleased("Pause"))
+		{
+			if (isInMenu) return;
+			if (this.Visible)
+			{
+				Hide();
+				GetTree().Paused = false;
+				return;
+			}
+			Show();
+			GetTree().Paused = true;
+		}
+	}
+
 	private void _on_Quit_pressed()
 	{
+		isInMenu = true;
 		popupMenu.PopupCentered();
 	}
 
 	private void _on_Settings_pressed()
 	{
+		isInMenu = true;
 		settingsPopup.PopupCenteredRatio(.7f);
+	}
+
+	private void _on_Settings_visibility_changed()
+	{
+		if(settingsPopup.Visible)
+		{
+			isInMenu= true;
+			return;
+		}
+		isInMenu = false;
 	}
 
 	private void _on_ConfirmationDialog_confirmed()
@@ -31,8 +61,15 @@ public partial class Pause : Control
 		GetTree().Quit();
 	}
 
+	private void _on_ConfirmationDialog_cancelled()
+	{
+		isInMenu= false;
+	}
+
 	private void _on_Resume_pressed()
 	{
+		Hide();
+		GetTree().Paused = false;
 		GD.Print("Resume");
 	}
 }
