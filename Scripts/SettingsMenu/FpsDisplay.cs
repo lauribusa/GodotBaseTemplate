40ac174f55d4c1b7d4f708555f05f9dfd2e0b45c
Lauri.busa <loki.lightbolt@gmail.com>
2023-01-26 21:19:08 +0100

    Prevent process when fpsDisplay is untoggled

--- Scripts/SettingsMenu/FpsDisplay.cs
+++ Scripts/SettingsMenu/FpsDisplay.cs
@@ -3,9 +3,9 @@ using System;
 
 public partial class FpsDisplay : Label
 {
-	public override void _Process(double delta)
-	{
-		if (!Visible) return;
-		this.Text = $"FPS: {Engine.GetFramesPerSecond()}";
-	}
+	public override void _Ready() => VisibilityChanged += OnVisibilityChanged;
+
+	public override void _Process(double delta) => Text = $"FPS: {Engine.GetFramesPerSecond()}";
+
+	private void OnVisibilityChanged() => SetProcess(Visible);
 }
